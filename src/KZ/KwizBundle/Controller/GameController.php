870b0e3571749a36e900f37bffe4b78e3c1376e1
Nicolas <lf.nicolas45@gmail.com>
2017-04-28 01:02:47 +0200

    Fin de partie Ok

--- src/KZ/KwizBundle/Controller/GameController.php
+++ src/KZ/KwizBundle/Controller/GameController.php
@@ -26,40 +26,41 @@ class GameController extends Controller
         $categories = $em->getRepository('KZKwizBundle:Category')->findAll();
         return $categories;
     }
+
     public function generateBoard(Party $party)
     {
         $board = [];
         //Génération des cases questions
-        for($i=0;$i<=26;$i++){
-            $board[$i]['category'] = rand(0,5);
+        for ($i = 0; $i <= 26; $i++) {
+            $board[$i]['category'] = rand(0, 5);
             $board[$i]['type'] = 'Q';
         }
         //Génération des cases bonus
-        for($i=27;$i<=29;$i++){
+        for ($i = 27; $i <= 29; $i++) {
             $board[$i]['type'] = 'B';
-            $board[$i]['category'] = rand(0,5);
+            $board[$i]['category'] = rand(0, 5);
         }
         //Génération des cases malus
-        for($i=30;$i<=32;$i++){
+        for ($i = 30; $i <= 32; $i++) {
             $board[$i]['type'] = 'M';
-            $board[$i]['category'] = rand(0,5);
+            $board[$i]['category'] = rand(0, 5);
         }
         //Génération des cases pièges
-        for($i=33;$i<=35;$i++){
+        for ($i = 33; $i <= 35; $i++) {
             $board[$i]['type'] = 'P';
-            $board[$i]['category'] = rand(0,5);
+            $board[$i]['category'] = rand(0, 5);
         }
         //Génération des cases aléatoires
-        for($i=36;$i<=38;$i++){
+        for ($i = 36; $i <= 38; $i++) {
             $board[$i]['type'] = 'A';
-            $board[$i]['category'] = rand(0,5);
+            $board[$i]['category'] = rand(0, 5);
         }
         //Génération de la prison
         $board[39]['type'] = 'J';
-        $board[39]['category'] = rand(0,5);
+        $board[39]['category'] = rand(0, 5);
         shuffle($board);
         $categories = $this->getCategories();
-        for($i=0;$i<=39;$i++){
+        for ($i = 0; $i <= 39; $i++) {
             $em = $this->getDoctrine()->getManager();
             $square = new Square();
             $square->setParty($party);
@@ -71,77 +72,94 @@ class GameController extends Controller
         }
         return $board;
     }
+
     function getBoard(Party $party)
     {
         $em = $this->getDoctrine()->getManager();
-        $board = $em->getRepository('KZKwizBundle:Square')->findBy(['party'=>$party]);
+        $board = $em->getRepository('KZKwizBundle:Square')->findBy(['party' => $party]);
         return $board;
     }
+
     public function isReady(Party $party)
     {
         $em = $this->getDoctrine()->getManager();
         $nbPlayer = $em->getRepository('KZKwizBundle:Party')->countNbPlayer($party);
-        if($nbPlayer==$party->getNbPlayer()){
+        if ($nbPlayer == $party->getNbPlayer()) {
             return true;
         }
         return false;
     }
+
     public function jsToPhp($id, $query)
     {
         $em = $this->getDoctrine()->getManager();
         $party = $em->getRepository('KZKwizBundle:Party')->find($id);
         return $query($party);
     }
+
     public function indexAction(Party $party)
     {
         $board = $this->getBoard($party);
-        if($this->isReady($party)){
-            if($board==NULL){
+        if ($this->isReady($party)) {
+            if ($board == NULL) {
                 $board = $this->generateBoard($party);
             }
         }
-        return $this->render('KZKwizBundle:Game:game.html.twig', ['board'=>$board]);
+        return $this->render('KZKwizBundle:Game:game.html.twig', ['board' => $board]);
     }
 
     public function endAction(Party $party)
     {
 
-        $square = $this->playerPositionAction($party, $this->getUser()->getId());
+        $squareActive = $this->playerPositionAction($party, $this->getUser()->getId());
 
-        if (41 == $square){
+        if (38 == $squareActive) {
 
             $em = $this->getDoctrine()->getManager();
 
-            $party = new Party();
-            $partyActive = $em->getRepository($party)->find($idParty);
-
-            //supprime les champs de la table Case qui ont l'id de la partie en cours
-            $square = new Square();
-            $squares = $em->getRepository($square)->findby(
-                array('party_id' => $idParty )
-            );
-
-            foreach ($squares as $squareDel){
-                $em->remove($squareDel);
-            }
             //Remplie la table history du User en cours
-            $history = new History();
-            $history->setUser($this->getUser());
-            $history->setParty($partyActive);
-            $history->setRank(1);
+            $games = $em->getRepository('KZKwizBundle:Game')->findBy(
+                array(
+                    'party' => $party,
+                ),
+                array(
+                    'square' => 'DESC'
+                ));
 
-            //Remplie la table history des autres Users
-            $game = new Game();
-            $games = $em->getRepository($game)->findBy(array(
-                    'party_id' => $idParty,
-            ));
+            $i = 1;
+            foreach ($games as $game) {
+                $history = new History();
+                $history->setUser($game->getUser());
+                $history->setParty($party);
+                $history->setRank($i);
+                $i++;
+                $em->persist($history);
+                $em->flush();
 
+
+            }
+
+
+            //supprime les champs de la table Game
+            foreach ($games as $game) {
+                $em->remove($game);
+                $em->flush();
+            }
+            //supprime les champs de la table Case qui ont l'id de la partie en cours
+            $squares = $em->getRepository('KZKwizBundle:Square')->findby(
+                array('party' => $party)
+            );
+            foreach ($squares as $squareDel) {
+                $em->remove($squareDel);
+                $em->flush();
+            }
 
 
         }
+        return 'hello';
     }
 
-    public function playerPositionAction(Party $party,$id)
+    public function playerPositionAction(Party $party, $id)
     {
         $em = $this->getDoctrine()->getManager();
 
